e73b74cf7e9c1c4f590ff1df350504a2f9d1029c
Mamadou WONE <mamadouwone12345@gmail.com>
2020-10-31 14:07:03 +0000

    First Day

---
+++ script/add.js
@@ -0,0 +1,6 @@
+let deleteB = document.querySelector('#deleteButton');
+let nameP = document.querySelector('#name');
+let priceP = document.querySelector('#price');
+deleteB.addEventListener('click', () => {
+    console.log(nameP.innerHTML);
+});
